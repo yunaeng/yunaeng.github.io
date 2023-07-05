5d3d251696b3ad302de9ab02867953bc5fe87d49
yunaeng <62501450+yunaeng@users.noreply.github.com>
2023-07-05 19:28:09 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ['0.jpeg', '1.jpeg', '2.jpeg', '3.jpg', '4.jpg']
+const images = ['0.jpeg', '1.jpeg', '2.jpeg', '3.jpg', '4.jpg', '5.jpg']
 
 const chosenimage = images[Math.floor(Math.random() * images.length)]
 
